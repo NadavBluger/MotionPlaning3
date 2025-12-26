fb14fdb93b7c8c94cf6f608c405c2fa3b98b4b25
nblug <nbluger@gmail.com>
2025-12-26 15:41:21 +0200

    .

--- RRTMotionPlanner.py
+++ RRTMotionPlanner.py
@@ -26,21 +26,24 @@ class RRTMotionPlanner(object):
             rand_config = self.bb.sample_random_config(goal_prob=self.goal_prob, goal=self.goal)
             self.extend(self.tree.get_nearest_config(rand_config)[1], rand_config)
 
-        plan = np.empty((0, 2))
         current = self.tree.get_vertex_for_config(self.goal)
-        while (current.config!=self.start).all():
-            plan=np.vstack((plan, current.config))
-            current =self.tree.vertices[self.tree.edges[self.tree.get_idx_for_config(current.config)]]
-        plan=np.vstack((plan, self.start))
-        return plan
+        plan = [current.config]
+        while np.any(current.config != self.start):
+            current = self.tree.vertices[self.tree.edges[self.tree.get_idx_for_config(current.config)]]
+            plan.append(current.config)
+
+        return np.array(plan)[::-1]
 
     def compute_cost(self, plan):
         '''
         Compute and return the plan cost, which is the sum of the distances between steps in the configuration space.
         @param plan A given plan for the robot.
         '''
-        #maybe need to return just the cost of the goal vertex
-        return sum(vertex.cost for vertex in plan)
+        # The cost of the plan is the cost of the goal vertex in the tree
+        cost=0
+        for i in range(len(plan)-1):
+            cost += self.bb.compute_distance(plan[i], plan[i+1])
+        return cost
 
 
     def extend(self, near_config: np.ndarray, rand_config: np.ndarray):
@@ -65,4 +68,3 @@ class RRTMotionPlanner(object):
             eid = self.tree.add_vertex(new_config)
             sid = self.tree.get_idx_for_config(near_config)
             self.tree.add_edge(sid, eid, self.bb.compute_distance(new_config, near_config))
-
